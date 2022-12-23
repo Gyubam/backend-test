87a2b1852be3858f9cfcb702b2069c93efdca354
sgb8170@naver.com <sgb8170@naver.com>
2022-12-23 13:34:13 +0900

    email 인증 기능 구현

--- src/main/java/com/example/backendtest/controller/HomeController.java
+++ src/main/java/com/example/backendtest/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.backendtest.controller;
 import com.example.backendtest.entity.User;
 import com.example.backendtest.provider.JwtTokenProvider;
 import com.example.backendtest.repository.UserRepository;
+import com.example.backendtest.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,6 +13,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -19,6 +22,8 @@ import java.security.Principal;
 @RestController
 @RequiredArgsConstructor
 public class HomeController {
+
+    private final EmailService emailService;
 
     @GetMapping("/")
     public String home() {
@@ -53,6 +58,14 @@ public class HomeController {
     @GetMapping("/user/v1")
     public String user() {
         return "test";
+    }
+
+    @PostMapping("/emailConfirm")
+    public String emailConfirm(@RequestParam String email) throws Exception {
+
+        String confirm = emailService.sendSimpleMessage(email);
+
+        return confirm;
     }
 
 
